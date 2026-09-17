2e5f104f8cc5a255ffae290ebdfa5ef9770a2797
SHADY-MU <shadyrizk57@gmail.com>
2026-09-17 17:26:30 +0300

    first commit

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -58,7 +58,7 @@ export default function Projects() {
         </p>
       </ScrollReveal>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch">
         {projects.map((project, index) => (
           <ScrollReveal
             key={project.id}
